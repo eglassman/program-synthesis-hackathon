0506df3dd530d5d9c63f8118a6307c191cd13dbd
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2017-01-22 13:52:13 -0800

    start implementing a witness function

--- sorta.Learning/Learners.cs
+++ sorta.Learning/Learners.cs
@@ -4,6 +4,7 @@ using Microsoft.ProgramSynthesis.Rules;
 using Microsoft.ProgramSynthesis.Learning;
 using Microsoft.ProgramSynthesis.Specifications;
 using System.Collections.Generic;
+using sorta.Semantics;
 
 namespace sorta.Learning
 {
@@ -13,11 +14,30 @@ namespace sorta.Learning
 
         // Your custom learning logic here (for example, witness functions)
 
+        ICollection<string> FindCharacters(string s) {
+            var chars = new HashSet<string>();
+            foreach(char cc in s) {
+                chars.Add(cc.ToString());
+            }
+            return chars;
+        }
 
         [WitnessFunction("FirstIndexOf", 1)]
 	    DisjunctiveExamplesSpec WitnessPositionPair(GrammarRule rule, ExampleSpec spec){
-                Console.WriteLine("witness called");
-                return null;
+                var nExamples = new Dictionary<State, IEnumerable<object>>();
+                foreach(State st in spec.ProvidedInputs) {
+                    var input = (Tuple<string, string>) st[rule.Body[0]];
+                    var output = (Order) spec.Examples[st];
+                    var needles = new List<string>();
+                    foreach(var needle in FindCharacters(input.Item1 + input.Item2)) {
+                        if(Semantics.Semantics.FirstIndexOf(input, needle) == output) {
+                            needles.Add(needle);
+                            Console.WriteLine("possible needle: " + needle);
+                        }
+                    }
+                    nExamples[st] = needles;
+                }
+                return DisjunctiveExamplesSpec.From(nExamples);
         }
     }
 }
