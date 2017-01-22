a8e1b801810024097f83d2f802f78c0aa2a3ba91
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2017-01-22 14:14:20 -0800

    add LastIndexOf and CountOf search criterias

--- sorta.Learning/Learners.cs
+++ sorta.Learning/Learners.cs
@@ -32,22 +32,38 @@ namespace sorta.Learning
             return new ExampleSpec(nExamples);
         }
 
-        [WitnessFunction(nameof(Semantics.Semantics.FirstIndexOf), 1)]
-	    DisjunctiveExamplesSpec WitnessFirstIndexOf(GrammarRule rule, ExampleSpec spec){
-                var nExamples = new Dictionary<State, IEnumerable<object>>();
+        delegate Order SearchCompareFun(Tuple<string, string> input, string needle);
+
+        DisjunctiveExamplesSpec WitnessSearchCompareFun(SearchCompareFun fun, GrammarRule rule, ExampleSpec spec) {
+            var nExamples = new Dictionary<State, IEnumerable<object>>();
                 foreach(State st in spec.ProvidedInputs) {
                     var input = (Tuple<string, string>) st[rule.Body[0]];
                     var output = (Order) spec.Examples[st];
                     var needles = new List<string>();
                     foreach(var needle in FindCharacters(input.Item1 + input.Item2)) {
-                        if(Semantics.Semantics.FirstIndexOf(input, needle) == output) {
+                        if(fun(input, needle) == output) {
                             needles.Add(needle);
-                            Console.WriteLine("possible needle: " + needle);
+                            //Console.WriteLine("possible needle: " + needle);
                         }
                     }
                     nExamples[st] = needles;
                 }
                 return DisjunctiveExamplesSpec.From(nExamples);
+        }
+
+        [WitnessFunction(nameof(Semantics.Semantics.FirstIndexOf), 1)]
+	    DisjunctiveExamplesSpec WitnessFirstIndexOf(GrammarRule rule, ExampleSpec spec){
+                return WitnessSearchCompareFun(Semantics.Semantics.FirstIndexOf, rule, spec); 
+        }
+
+        [WitnessFunction(nameof(Semantics.Semantics.LastIndexOf), 1)]
+	    DisjunctiveExamplesSpec WitnessLastIndexOf(GrammarRule rule, ExampleSpec spec){
+                return WitnessSearchCompareFun(Semantics.Semantics.LastIndexOf, rule, spec); 
+        }
+
+        [WitnessFunction(nameof(Semantics.Semantics.CountOf), 1)]
+	    DisjunctiveExamplesSpec WitnessCountOf(GrammarRule rule, ExampleSpec spec){
+                return WitnessSearchCompareFun(Semantics.Semantics.CountOf, rule, spec); 
         }
     }
 }
--- sorta.Semantics/Semantics.cs
+++ sorta.Semantics/Semantics.cs
@@ -20,6 +20,24 @@ namespace sorta.Semantics
             return cmp(inp.Item1.IndexOf(s), inp.Item2.IndexOf(s));
         }
 
+        public static Order LastIndexOf(Tuple<string, string> inp, String s) {
+            return cmp(inp.Item1.LastIndexOf(s), inp.Item2.LastIndexOf(s));
+        }
+
+        private static int SubstringCount(string haystack, string needle) {
+            int count = 0;
+            for (int i = haystack.IndexOf(needle, StringComparison.Ordinal);
+                     i >= 0;
+                     i = haystack.IndexOf(needle, i + 1, StringComparison.Ordinal)) {
+                         count++;
+                     }
+            return count;
+        }
+
+        public static Order CountOf(Tuple<string, string> inp, String s) {
+            return cmp(SubstringCount(inp.Item1, s) , SubstringCount(inp.Item2, s));
+        }
+
         public static Order Invert(Order order) {
             return (order == Order.Less)? Order.Greater :
                    (order == Order.Greater)? Order.Less : Order.Equal;
